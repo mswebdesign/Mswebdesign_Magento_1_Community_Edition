debca359e412726fbb6c95bc0b86132346cb18dc
Christian Grugel <christian@grugel.net>
2016-11-16 14:20:37 +0100

    Magento CE 1.9.3.1

--- app/code/core/Mage/Catalog/Model/Category/Api.php
+++ app/code/core/Mage/Catalog/Model/Category/Api.php
@@ -20,7 +20,7 @@
  *
  * @category    Mage
  * @package     Mage_Catalog
- * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -468,7 +468,7 @@ class Mage_Catalog_Model_Category_Api extends Mage_Catalog_Model_Api_Resource
     {
         $category = $this->_initCategory($categoryId);
         $positions = $category->getProductsPosition();
-        $productId = $this->_getProductId($productId, $identifierType);
+        $productId = $this->_getProductId($productId);
         $positions[$productId] = $position;
         $category->setPostedProducts($positions);
 
